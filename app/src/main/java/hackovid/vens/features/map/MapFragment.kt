9a7d93d9b45f70875ec97f2004f8c4096e2c4340
Iris Serrano <iris.serranoc@gmail.com>
2020-04-10 18:44:51 +0200

    request location permission

--- app/src/main/java/hackovid/vens/features/map/MapFragment.kt
+++ app/src/main/java/hackovid/vens/features/map/MapFragment.kt
@@ -1,18 +1,24 @@
 package hackovid.vens.features.map
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.core.view.doOnLayout
 import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import hackovid.vens.R
 import hackovid.vens.common.ui.BaseFragment
 import hackovid.vens.common.utils.observe
 import hackovid.vens.databinding.FragmentMapBinding
-import kotlin.math.roundToInt
-import kotlinx.android.synthetic.main.fragment_map.view.infoLayout
+import kotlinx.android.synthetic.main.fragment_map.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.roundToInt
 
 class MapFragment : BaseFragment<FragmentMapBinding>(), OnMapReadyCallback {
     override val layoutRes = R.layout.fragment_map
@@ -26,6 +32,7 @@ class MapFragment : BaseFragment<FragmentMapBinding>(), OnMapReadyCallback {
         binding.viewModel = viewModel
         binding.setupViews()
         subscribeUi()
+        fetchLocation()
         setupMap()
     }
 
@@ -44,6 +51,12 @@ class MapFragment : BaseFragment<FragmentMapBinding>(), OnMapReadyCallback {
                 googleMap.setBottomPadding(padding)
             }
         }
+
+        observe(viewModel.location) {
+            if (this::googleMap.isInitialized) {
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(it))
+            }
+        }
     }
 
     private fun setupMap() {
@@ -58,11 +71,65 @@ class MapFragment : BaseFragment<FragmentMapBinding>(), OnMapReadyCallback {
         this.googleMap = googleMap
         googleMap.setOnMapLongClickListener { latLng -> viewModel.location.value = latLng }
         viewModel.location.value?.let { latLng ->
-            googleMap.moveCamera(CameraUpdateFactory.zoomTo(12f))
+            googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
         }
         mapBottomPadding.value?.let { padding -> googleMap.setBottomPadding(padding) }
+
+        if (context?.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED &&
+            context?.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED)
+
+            googleMap.isMyLocationEnabled = true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode) {
+            REQ_CODE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    fetchLocation()
+                }
+            }
+        }
+    }
+
+    private fun fetchLocation() {
+
+        if(context?.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED
+            || context?.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            requestPermissions(permissions, REQ_CODE_LOCATION)
+            return
+        }
+
+        val locationManager: LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val providers: List<String> = locationManager.getProviders(true)
+        var location: Location? = null
+
+        for (i in providers.size - 1 downTo 0) {
+            location = locationManager.getLastKnownLocation(providers[i])
+            if (location != null) break
+        }
+
+        location?.let { loc ->
+            LatLng(loc.latitude, loc.longitude)
+            viewModel.location.value = LatLng(loc.latitude, loc.longitude)
+        }
+
+        if (this::googleMap.isInitialized) googleMap.isMyLocationEnabled = true
     }
 
     private fun GoogleMap.setBottomPadding(padding: Int) = setPadding(0, 0, 0, padding)
+
+    companion object {
+        const val REQ_CODE_LOCATION = 100
+    }
 }
