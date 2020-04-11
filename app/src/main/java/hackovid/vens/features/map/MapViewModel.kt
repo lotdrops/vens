d2305fd0485b493a0623f3eddf1def137444f77c
Iris Serrano <iris.serranoc@gmail.com>
2020-04-11 22:38:55 +0200

    set favourite icon on store map detail

--- app/src/main/java/hackovid/vens/features/map/MapViewModel.kt
+++ app/src/main/java/hackovid/vens/features/map/MapViewModel.kt
@@ -1,24 +1,29 @@
 package hackovid.vens.features.map
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.map
+import androidx.lifecycle.*
 import com.google.android.gms.maps.model.LatLng
-import hackovid.vens.common.data.Store
 import hackovid.vens.common.data.StoreDao
+import kotlinx.coroutines.launch
 
-class MapViewModel(storeDao: StoreDao) : ViewModel() {
+class MapViewModel(private val storeDao: StoreDao) : ViewModel() {
     val stores = storeDao.getAllByName()
     val location: MutableLiveData<LatLng?> = MutableLiveData(LatLng(DEFAULT_LAT, DEFAULT_LONG))
 
-    val selectedStore = MutableLiveData<Store>()
-    val showStoreInfo = selectedStore.map { it != null }
+    val selectedStoreId = MutableLiveData<Int>()
+    val selectedStore = selectedStoreId.switchMap { storeDao.getStoreById(it) }
+    val showStoreInfo = selectedStoreId.map { it != null }
 
     fun onStoreActionClicked() {
     }
 
     fun onBackPressed(): Boolean = (showStoreInfo.value == true).apply {
-        if (this) selectedStore.value = null
+        if (this) selectedStoreId.value = null
+    }
+
+    fun onFavouriteClicked() {
+        viewModelScope.launch {
+            selectedStore.value?.let { st -> storeDao.setFavourite(st.id, !st.isFavourite) }
+        }
     }
 }
 private const val DEFAULT_LAT = 41.384771
