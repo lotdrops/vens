07e705e99233d3da993346aab8b4732fa5e0934f
Iris Serrano <iris.serranoc@gmail.com>
2020-05-10 12:08:36 +0200

    Locate store on map

---
+++ app/src/main/java/hackovid/vens/features/onboarding/di/OnBoardingSharedViewModel.kt
@@ -0,0 +1,8 @@
+package hackovid.vens.features.onboarding.di
+
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+
+class OnBoardingSharedViewModel :ViewModel() {
+    var onMapResult: LatLng? = null
+}
--- app/src/main/java/hackovid/vens/features/onboarding/ui/OnBoardingActivity.kt
+++ app/src/main/java/hackovid/vens/features/onboarding/ui/OnBoardingActivity.kt
@@ -3,8 +3,12 @@ package hackovid.vens.features.onboarding.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import hackovid.vens.R
+import hackovid.vens.features.onboarding.di.OnBoardingSharedViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OnBoardingActivity : AppCompatActivity() {
+
+    private val viewModel: OnBoardingSharedViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
