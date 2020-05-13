3f6a5784de09e8888d62967e5c0119515e45058f
IvannPascual <pascualpalaciosivan@gmail.com>
2020-05-13 20:17:50 +0200

    Update database, and types and subtypes
    Co-authored-by: ivan.pascual <ivan.pascual@basetis.com>
    Co-authored-by: Jordi Saumell <jsaumell420@gmail.com>

--- app/src/main/java/hackovid/vens/common/data/filter/FilterParams.kt
+++ app/src/main/java/hackovid/vens/common/data/filter/FilterParams.kt
@@ -15,7 +15,7 @@ data class FilterParams(
         const val ANY_DISTANCE = 2
         const val DEFAULT_CROWD = 0
         fun defaultCategories() =
-            StoreType.values().map { it == StoreType.GROCERY || it == StoreType.MARKET }
+            StoreType.values().map { it == StoreType.FOOD || it == StoreType.MARKET }
         fun defaultSelectable() =
             listOfFalse(NUMBER_OF_SELECTABLE)
     }
--- app/src/main/java/hackovid/vens/features/detail/DetailViewModel.kt
+++ app/src/main/java/hackovid/vens/features/detail/DetailViewModel.kt
@@ -46,18 +46,26 @@ class DetailViewModel(
 
     private fun StoreType.imageDetail(): Int {
         return when (store.value?.store?.type) {
-            StoreType.GROCERY -> R.drawable.grocery
-            StoreType.BEAUTY -> R.drawable.beauty
-            StoreType.MALL -> R.drawable.mall
-            StoreType.GALLERY -> R.drawable.mall
-            StoreType.HOME -> R.drawable.home
-            StoreType.MARKET -> R.drawable.market
-            StoreType.FASHION -> R.drawable.fashion
-            StoreType.LEISURE -> R.drawable.leisure
-            StoreType.EVERYDAY -> R.drawable.everyday
-            StoreType.SERVICES -> R.drawable.services
-            StoreType.OTHER -> R.drawable.other
-            null -> R.drawable.grocery
+            StoreType.BAKERY_PASTRY_DAIRY -> R.drawable.bakery
+            StoreType.DRINKS -> R.drawable.drinks
+            StoreType.EGGS_AND_BIRDS-> R.drawable.eggs_birds
+            StoreType.FASHION-> R.drawable.fashion
+            StoreType.FISH_AND_SEA_FOOD-> R.drawable.fish_sea_food
+            StoreType.FOOD-> R.drawable.food
+            StoreType.FRUIT_AND_VEGETABLES-> R.drawable.fruits_and_vegetables
+            StoreType.HEALTH-> R.drawable.health
+            StoreType.HOME-> R.drawable.home
+            StoreType.LEISURE_AND_CULTURE-> R.drawable.leisure_culture
+            StoreType.LOOK-> R.drawable.look
+            StoreType.MARKET-> R.drawable.market
+            StoreType.MEAT-> R.drawable.meat
+            StoreType.OTHER-> R.drawable.others
+            StoreType.PREPARED_DISHES-> R.drawable.prepared_dishes
+            StoreType.SERVICES-> R.drawable.services
+            StoreType.SHOPPING_CENTER-> R.drawable.comercial_gallery
+            StoreType.SHOPPING_GALLERY-> R.drawable.comercial_gallery
+            StoreType.SUPERMARKET-> R.drawable.supermarket
+            else -> R.drawable.others
         }
     }
 }
